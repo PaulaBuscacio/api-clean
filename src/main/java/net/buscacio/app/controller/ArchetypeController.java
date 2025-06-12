d40570602dd05f023e66754bd6c374c311aa853a
PaulaBuscacio <paula@buscacio.net>
2025-06-11 22:37:02 -0300

    cors configurado

--- src/main/java/net/buscacio/app/controller/ArchetypeController.java
+++ src/main/java/net/buscacio/app/controller/ArchetypeController.java
@@ -7,10 +7,7 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -20,7 +17,7 @@ import java.io.IOException;
 public class ArchetypeController {
 
     private final ArchetypeService archetypeService;
-
+    @CrossOrigin
     @PostMapping
     public ResponseEntity<?> generateProject(@RequestBody ArchetypeRequestDTO requestDTO) throws IOException, InterruptedException {
 
